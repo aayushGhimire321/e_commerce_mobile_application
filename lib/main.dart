79ff8f0f9e977ca83e2daba026986456a96185a5
aayushghimire321 <aghimire491@gmail.com>
2024-09-10 21:33:44 +0545

    works started

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:e_commerce_application/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
-
+import 'package:e_commerce_application/utils/theme/theme.dart';
 void main() {
   runApp(const App());
 }
@@ -11,8 +12,8 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.system,
-      theme: ThemeData(),
-      darkTheme: ThemeData(),
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
     );
   }
 }
